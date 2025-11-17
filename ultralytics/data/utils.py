9e2917b1d455c04abee3decbfd63b33a1b53e810
Yakuho <Yakuho@foxmail.com>
2025-11-17 11:21:01 +0800

    fix label:0 miss bug

--- ultralytics/data/utils.py
+++ ultralytics/data/utils.py
@@ -222,7 +222,7 @@ def verify_image_label(args: tuple, fmt="YOLO", names: dict=None, name_error="wa
                     assert names is not None, "label format labelme must set names like: {\"label-1\": 0, ...}"
                     lb = []
                     for item in json.load(f)["shapes"]:
-                        if label := names.get(item["label"]):
+                        if (label := names.get(item["label"])) is not None:
                             lb.append([label] + [np.clip(v / s, 0, 1) for xy in item["points"] for v, s in zip(xy, shape[::-1])])
                         elif label is None and name_error in ("warn", "warning"):
                             warnings.warn(f"label <{label}> skipped due to not found in names mapping.")
